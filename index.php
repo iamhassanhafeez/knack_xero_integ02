d70328a7c9332306a1e47195d3d534e8a4d18eec
Hassan Hafeez <hasancheeta@gmail.com>
2024-09-05 11:48:14 +1200

    changed redirect URL for Xero as I renamed the folder and using localhost for development

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ session_start();
 //============ Xero Config ==================
 $clientId = 'DB8962151A3D4C339A0D4B1E12712771';
 $clientSecret = 'hnXTTDTWyKi4Crhw2TEPnxmyP2qX92TH-HoCadvanaVX9w-P';
-$redirectUri = 'http://localhost/xero-app-new/index.php';
+// old $redirectUri = 'http://localhost/xero-app-new/index.php';
+$redirectUri = 'http://localhost/knack_xero_integration/index.php';
 
 $provider = new \League\OAuth2\Client\Provider\GenericProvider([
     'clientId'                => $clientId,   
